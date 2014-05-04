4a0c19fedb74d4d3b1dfa0ecf33bd39c8f650334
Maelstroms38 <stroms38@gmail.com>
2014-05-04 11:35:00 -0400

    Initial Commit

---
+++ Photo Bombers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photo Bombers
+//
+//  Created by Michael Stromer on 5/4/14.
+//  Copyright (c) 2014 Michael Stromer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photo Bombers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photo Bombers
+//
+//  Created by Michael Stromer on 5/4/14.
+//  Copyright (c) 2014 Michael Stromer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
